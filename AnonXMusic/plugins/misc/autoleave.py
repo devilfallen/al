3576571708d477923335c16b046531f2df3c7f9a
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-03 22:53:40 +0300

    Update autoleave.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001891680771
+                                and i.chat.id != -1001923310875
                             ):
                                 if left == 20:
                                     continue
