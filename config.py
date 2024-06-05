fed14427181dbe2794524d650d899d836188d1e4
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-06 01:02:24 +0300

    Update config.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/SohbetAlemi",
+    "https://t.me/ByCedric",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
