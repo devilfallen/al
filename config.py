5af2028b81654c6afbe9d96a83be55198e6595e3
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-06 01:03:18 +0300

    Update config.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
+    "CEDRİCO",
     "https://t.me/ByCedric",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
